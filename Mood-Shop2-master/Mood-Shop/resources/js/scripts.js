c0d8aa7a0e7568dc80e0edb979a0eb1c3098733c
Smerly <69550699+Smerly@users.noreply.github.com>
2020-09-29 22:37:34 -0700

    Fixed another issue I was having

--- Mood-Shop2-master/Mood-Shop/resources/js/scripts.js
+++ Mood-Shop2-master/Mood-Shop/resources/js/scripts.js
@@ -2,44 +2,37 @@ import data from './data.js'
 const itemsContainer = document.getElementById('items')
 
 for (let i=0; i<data.length; ++i) {
-  
     let newDiv = document.createElement('div');
-    newDiv.className = 'item'
-
- 
-    let img = document.createElement('img');
+      newDiv.className = 'item'
     
+    let img = document.createElement('img');
     img.src = data[i].image
     img.width = 300
     img.height = 300
-
-   
     newDiv.appendChild(img)
-
-    itemsContainer.appendChild(newDiv)
-
+  
     let desc = document.createElement('P')
-
+    desc.innerText =data[i].desc
     newDiv.appendChild(desc)
-
-    desc.innerText =data[i].desc    
 
     let price = document.createElement('P')
     price.innerText = data[i].price
     newDiv.appendChild(price)
 
+  
     let button = document.createElement('button')
-
     button.id = data[i].name
-
+  
+  
     button.dataset.price = data[i].price
     button.innerHTML = "Add to Cart"
     newDiv.appendChild(button)
+
+  
     itemsContainer.appendChild(newDiv)
+  }
 
-}
 
-const cart [ ]
 
 function addItem (name, price) {
     for (let i = 0; i < cart.length; i += 1) {
--- Mood-Shop2-master/resources/js/scripts.js
+++ Mood-Shop2-master/resources/js/scripts.js
@@ -1,44 +1,85 @@
-const itemsContainer = document.getElementById('items')
 import data from './data.js'
-
+const itemsContainer = document.getElementById('items')
 
 for (let i=0; i<data.length; ++i) {
-  
     let newDiv = document.createElement('div');
-    newDiv.className = 'item'
-
- 
-    let img = document.createElement('img');
+      newDiv.className = 'item'
     
+    let img = document.createElement('img');
     img.src = data[i].image
     img.width = 300
     img.height = 300
-
-   
     newDiv.appendChild(img)
+  
+    let desc = document.createElement('P')
+    desc.innerText =data[i].desc
+    newDiv.appendChild(desc)
 
+    let price = document.createElement('P')
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+
+  
+    let button = document.createElement('button')
+    button.id = data[i].name
+  
+  
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+
+  
     itemsContainer.appendChild(newDiv)
+  }
+
+
+
+function addItem (name, price) {
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            cart[i].qty += 1
+            return
+        }
+    }
+    const item = { name, price, qty: 1 }
+    cart.push(item)
+
+
+}
+// Show Items
+function showItems () {
+    const qty = getQty()
+    console.log(`You have ${qty} items in your cart`)
+    for (let i = 0; i < cart.length; i += 1) {
+        console.log(`-${ cart[i].name} $${cart[i].price} x ${cart[i].qty} `)
+    }
+    const total = getTotal()
+    console.log(`Your total is: $${total.toFixed(2)}`)
+}
+
+// Get Quantity
+function getQty() {
+    let qty = 0
+    for (let i = 0; i < cart.length; i += 1) {
+        qty += cart[i].qty
+    }
+    return qty
+}
+
+// Get the Total
+function getTotal() {
+    let total = 0
+    for (let i = 0; i < cart.length; i += 1) {
+        total = total + `${cart[i].price} * ${cart[i].qty}`
+    }
+
 }
 
 
-let desc = document.createElement('P')
+addItem(happy, 14.99)
+addItem(sad, 13.99)
 
-desc.innerText =data[i].desc
-
-newDiv.appendChild(desc)
-
-let price = document.createElement('P')
-price.innerText = data[i].price
-newDiv.appendChild(price)
-
-
-
-
-
-
-
-
-
+showItems()
 
 
 
