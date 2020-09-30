afef1af9c29b1dab8624c3920f6e03d2104d0ff6
Smerly <69550699+Smerly@users.noreply.github.com>
2020-09-30 00:58:02 -0700

    functional (Except for one thing, cant figure out)

--- Mood-Shop2-master/resources/js/scripts.js
+++ Mood-Shop2-master/resources/js/scripts.js
@@ -5,6 +5,10 @@ itemList.innerHTML = '<li> Hello World</li>'
 const cartQty = document.getElementById('cart-qty')
 const cartTotal = document.getElementById('cart-total')
 
+const addForm = document.getElementById('add-form')
+const itemName =  document.getElementById('item-name')
+const itemPrice =  document.getElementById('item-price')
+
 
 
 for (let i=0; i<data.length; ++i) {
@@ -39,7 +43,8 @@ for (let i=0; i<data.length; ++i) {
   }
 
 const all_items_button = document.querySelectorAll("button")
-const all_items_button = Array.from(document.querySelectorAll("button"))
+// const all_items_button = Array.from(document.querySelectorAll("button"))
+
 
 all_items_button.forEach(elt => elt.addEventListener('click', () => {
     addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
@@ -51,6 +56,30 @@ all_items_button.forEach(elt => elt.addEventListener('click', () => {
 
 
 const cart = []
+
+itemList.onchange = function(e) {
+    if (e.target && e.target.classList.contains('update')) {
+        const name = e.target.dataset.name
+        const qty = parseInt(e.target.value)
+        updateCart(name, qty)
+    }
+
+}
+
+itemList.onclick = function(e) {
+  //  console.log("Clicked List")
+   // console.log(e.target)
+    if (e.target && e.target.classList.contains('remove')) {
+        const name = e.target.dataset.name
+        removeItem(name)
+
+    } else if (e.target && e.target.classList.contains('add-one')) {
+        const name = e.target.dataset.name
+        addItem(name)
+    } else if (e.target && e.target.classList.contains('remove-one')) {
+        removeItem(name, 1)
+    }
+}
 
 function addItem (name, price) {
     for (let i = 0; i < cart.length; i += 1) {
@@ -75,7 +104,22 @@ function showItems () {
       //  { name:'Apple', price: 0.99, qty: 3}
 
         const {name, price, qty} = cart[i]
-        itemStr += `<li> ${name} $${price} x ${qty} = ${qty * price} </li>`
+        itemStr += 
+        `<li> ${name} $${price} x ${qty} = ${qty * price} 
+        <button class='remove' data-name='${name}>
+        Remove
+        </button> 
+
+        <button class='add-one' data-name='${name}>
+        +
+        </button> 
+        
+
+        <button class='remove-one' data-name='${name}>
+        -
+        </button>
+        <input class='update' type='number' data-name='${name}>
+        </li>`
     }
 
     itemList.innerHTML = itemStr 
@@ -110,17 +154,27 @@ function removeItem(name, qty = 0) {
             if (qty > 0) {
                 cart[i].qty -= qty
             }
-
-            
             if (cart[i].qty < 1 || qty === 0) {
                 cart.splice(i, 1)
             }
-
+            showItems()
             return 
         }
     }
 }
 
+function updateCart(name, qty) {
+    for (let i = 0; i < cart.length; i += 1) {}
+        if (cart[i].name === name) {
+            if (qty < 1) {
+                removeItem(name)
+                return 
+            }
+            cart[i].qty = qty
+            showItems()
+            return
+        }
+}
 
 addItem(happy, 14.99)
 addItem(sad, 13.99)
